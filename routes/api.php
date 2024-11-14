65c93f985613fe02860c33498e2dd18f5a4caddb
EstebanPerdomo11 <estebanperdomo2003@gmail.com>
2024-11-14 18:14:56 -0500

    Se modificó la función de loginUser de loginController para el tema de los roles

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\jugadoresController;
-use App\Http\Controllers\EstadisticasJugadorController;
+use App\Http\Controllers\Api\loginController;
 
 // Rutas de jugadores
 Route::get('/jugadores', [jugadoresController::class, 'index']);
 Route::get('/jugadores/{id}', [jugadoresController::class, 'show']);
-Route::post('/login', [jugadoresController::class, 'loginUser']);
 Route::post('/jugadores', [jugadoresController::class, 'store']);
 Route::put('/jugadores/{id}', [jugadoresController::class, 'update']);
 Route::patch('/jugadores/{id}', [jugadoresController::class, 'updatePartial']);
 Route::delete('/jugadores/{id}', [jugadoresController::class, 'eliminar']);
 
-// Ruta para agregar estadísticas de jugador
-Route::post('/estadisticas', [EstadisticasJugadorController::class, 'index']);
-Route::post('/estadisticas', [EstadisticasJugadorController::class, 'store']);
+Route::get('/login', [loginController::class, 'logueo']);
+Route::post('/login', [loginController::class, 'loginUser']);
+Route::post('/logout', [loginController::class, 'logout']);
+
+
+
+
